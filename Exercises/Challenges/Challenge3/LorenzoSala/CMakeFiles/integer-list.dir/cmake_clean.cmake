94f22878c7708c498ef8e03f3444d650ea37c4a2
816733 <lorenzo3.sala@mail.polimi.it>
2015-04-21 17:27:07 +0200

    Update to Challenge3

---
+++ Exercises/Challenges/Challenge3/LorenzoSala/CMakeFiles/integer-list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/integer-list.dir/integer-list.cpp.o"
+  "integer-list.pdb"
+  "integer-list"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/integer-list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
