89f5fff9b853a2dc9c6f42c421e67a34f24db4e5
816733 <lorenzo3.sala@mail.polimi.it>
2015-05-04 17:52:11 +0200

    Update to Challenge 2

--- Exercises/Challenges/Challenge2/LorenzoSala/main_rk.cpp
+++ Exercises/Challenges/Challenge2/LorenzoSala/main_rk.cpp
@@ -1,5 +1,4 @@
-#include "rk23.hpp"
-#include "rk45.hpp"
+#include "rk.hpp"
 #include "muParserInterface.hpp"
 #include <iostream>
 #include <fstream>
@@ -11,19 +10,6 @@ void printAndExit()
   std::cout<<"Something wrong with input"<<std::endl;
   std::exit(1);
 }
-
-std::vector<std::pair<double,double>> 
-    rk(std::function<double (double const &, double const &)> const & fun,
-	 double const & t0,
-	 double const & T,
-	 double const & y0,
-	 double const & h_init, 
-	 double const & h_max, 
-	 double const & final_error,
-	 int & status,
-	 Flag const & flag,
-	 std::size_t const & maxSteps);
-
 
 int main()
 {
@@ -50,11 +36,14 @@ int main()
   // auto fun = [](double const & t, double const & y){return -std::sin(t);};
   // cout << fun << endl;
   
-  double t0=0;
-  double y0=1;
-  double T=100;
-  double h_init=0.2;
-  double errorDesired=1.e-5;
+  Parametri data;
+  data.t0=0;
+  data.y0=1;
+  data.T=100;
+  data.h_init=0.2;
+  data.errorDesired=1.e-5;
+  data.maxsteps = 10000;
+
   int status;
   Flag flag;  
   while(1){
@@ -62,28 +51,28 @@ int main()
         char p;
         cin >> p;
 	if(p == '0'){
-		flag.RKmethod = 0;
+		flag.RKmethod = RK23;
 		break;
 	}else if(p == '1'){
-		flag.RKmethod = 1;
+		flag.RKmethod = RK45;
 		break;
 	}
   }
   while(1){
-  	cout << "Solve with cutting half timestep or optimal timestep? [0 for CHTS - 1 for OTS]" << endl;
+  	cout << "Solve with cutting half timestep or optimal timestep? [0/CHTS for CHTS - 1/OTS for OTS]" << endl;
 	char p;
         cin >> p;
 	if(p == '0'){
-		flag.timestep = 0;
+		flag.timestep = CHTS;
 		break;
 	}else if(p == '1'){
-		flag.timestep = 1;
+		flag.timestep = OTS;
 		break;
 	}
   }
 
   std::vector<std::pair<double,double>> result= 
-    rk(fun,t0,T,y0,h_init,(T-t0)/4.,errorDesired,status,flag,10000);
+    rk(fun,data,status,flag);
 
   ofstream file("result.dat");
 
@@ -115,40 +104,14 @@ int main()
 
   	double max_error(0);
   	for (auto i : result)
-    		max_error=std::max(max_error,std::abs(exact(i.first)-i.second));
+    		max_error = std::max(max_error,std::abs(exact(i.first)-i.second));
   	std::cout.setf(std::ios::scientific);
-  	std::cout<<"Max error "<<max_error<<" Desired max error "<<errorDesired;
+  	std::cout<<"Max error "<<max_error<<" Desired max error " << data.errorDesired;
   	std::cout<<std::endl;
-  	if(max_error > errorDesired)
+  	if(max_error > data.errorDesired)
 		std::cout << "Not a good convergence, try with optimal timestep [OTS]" << std::endl;
   }
 
   return 0;
 }
-
-
-std::vector<std::pair<double,double>> 
-    rk(std::function<double (double const &, double const &)> const & fun,
-	 double const & t0,
-	 double const & T,
-	 double const & y0,
-	 double const & h_init, 
-	 double const & h_max, 
-	 double const & final_error,
-	 int & status,
-	 Flag const & flag,
-	 std::size_t const & maxSteps){
-  
-	std::vector<std::pair<double,double>> result;
-	
-	if (flag.RKmethod == 0){
-		result = ODE::rk23(fun,t0,T,y0,h_init,h_max,final_error,status,flag,maxSteps);
-		std::cout << "Solved with RK23" << std::endl;
-	}else if(flag.RKmethod == 1){
-		result = ODE::rk45(fun,t0,T,y0,h_init,h_max,final_error,status,flag,maxSteps);
-		std::cout << "Solved with RK45" << std::endl;
-	}
-	return result;
-};
-
 
